7c68933f52f660f4345b4b2af00fb5708dad5667
Anna Chernova <annanvks@gmail.com>
2024-01-16 22:45:30 +0000

    add function to fetch data by city name

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,48 @@
-// - Create a weather dashboard with form inputs.
-//   - When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history
 const APIkey = "5fe82d252dd8fa6c70f89de72b7ab0bd";
-
-const cityName = $("#search-input").val().trim();
-
 const limit = 6;
 
 $("#search-button").on("click", function (event) {
   event.preventDefault();
-  console.log(cityName);
+  //find the city name
+  let cityName = $("#search-input").val().trim();
+  if (cityName.length < 2) {
+    alert("City name must be at least 3 letter long");
+    return;
+  }
+  callApi(cityName);
 });
 
-createQueryUrl();
+function callApi(cityName) {
+  //creating a query url
+  let queryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${APIkey}`;
 
-function createQueryUrl() {
-  const queryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${APIkey}`;
+  // an asynchronous request to retrieve JSON data
+  $.getJSON(queryUrl)
 
-  console.log(queryUrl);
-  console.log("I am here");
+    //if request is successful - execute callback function
+    .done(function (geoData) {
+      if (geoData.length !== 1) {
+        alert("Can not find such city, try again, please.");
+        return;
+      }
+      //todo: check that the city is the expected one and not just some random
+      console.log(`found city ${JSON.stringify(geoData[0])}`);
+
+      //creating a new URL for the second API call - with latitude and longitude from the geoData
+      queryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${geoData[0]["lat"]}&lon=${geoData[0]["lon"]}&appid=${APIkey}`;
+
+      $.getJSON(queryUrl)
+        .done(function (weatherData) {
+          console.log(`Got weatherData ${JSON.stringify(weatherData)}`);
+          // todo: got weather data, add city to the list
+        })
+        //If the Weather API call fails - an alert is shown
+        .fail(function () {
+          alert("Could not get city data");
+        });
+    })
+    //If the Geo API call fails - an alert is shown
+    .fail(function () {
+      alert("Could not find such city");
+    });
 }
-
-// function searchForCity() {
-
-// }
-
-//   - When a user views the current weather conditions for that city they are presented with:
-//     - The city name
-//     - The date
-//     - An icon representation of weather conditions
-//     - The temperature
-//     - The humidity
-//     - The wind speed
-//   - When a user view future weather conditions for that city they are presented with a 5-day forecast that displays:
-//     - The date
-//     - An icon representation of weather conditions
-//     - The temperature
-//     - The humidity
-//   - When a user click on a city in the search history they are again presented with current and future conditions for that city
