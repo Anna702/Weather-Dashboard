05d9839677f6f7e955bd4985f485530dc1c00449
Anna Chernova <annanvks@gmail.com>
2024-01-17 00:50:54 +0000

    add function to update history list + clear

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,9 @@ $("#search-button").on("click", function (event) {
   callApi(cityName);
 });
 
+let uniqueCities = [];
+
+//fetch all the necessary data and display
 function callApi(cityName) {
   //creating a query url
   let queryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${APIkey}`;
@@ -33,34 +36,39 @@ function callApi(cityName) {
 
       $.getJSON(queryUrl)
         .done(function (weatherData) {
-          const dataWeather = `${JSON.stringify(weatherData)}`;
-          console.log(`it is a dataweather: ${dataWeather}`);
-
           //add fetched data to the screen
-
           const todayDate = "   (" + dayjs().format("DD/M/YYYY") + ")";
 
+          //weather icon
           const icon = weatherData.list[0].weather[0].icon;
           const iconSrc = `https://openweathermap.org/img/wn/${icon}@2x.png`;
           const iconImg = $("<img>").attr("src", iconSrc);
 
+          //header for a main forecast block
           const mainInfo = cityName + todayDate;
 
+          //temperature, wind, humidity
           const temperature = $("<p>").text(
             `Temp: ${Math.round(weatherData.list[0].main.temp - 273.15)}°C`
           );
-
           const wind = $("<p>").text(
             `Wind: ${weatherData.list[0].wind.speed} KPH`
           );
-
           const humidity = $("<p>").text(
             `Humidity: ${weatherData.list[0].main.humidity} %`
           );
 
+          // Remove searched city from #today
+          $("#today").empty();
+
           $("#today").append(mainInfo, iconImg, temperature, wind, humidity);
           $("#today").addClass("today-weather");
-          // todo: got weather data, add city to the list
+
+          // Add city to the history list - only if it is unique
+          if (!uniqueCities.includes(cityName)) {
+            uniqueCities.push(cityName);
+            updateHistoryList();
+          }
         })
         //If the Weather API call fails - an alert is shown
         .fail(function () {
@@ -71,4 +79,23 @@ function callApi(cityName) {
     .fail(function () {
       alert("Could not find such city");
     });
+}
+
+function updateHistoryList() {
+  // Clear + update history city list
+  $("#history").empty();
+
+  uniqueCities.forEach(function (city) {
+    const cityList = $("<a>")
+      .addClass("list-group-item list-group-item-action")
+      .text(city)
+      .attr("href", "#");
+    $("#history").append(cityList);
+
+    // Handle the click event on the city list item
+    cityList.on("click", function (event) {
+      event.preventDefault();
+      callApi(city);
+    });
+  });
 }
